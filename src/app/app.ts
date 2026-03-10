6ac98b8f91920aa6a7f4e620d8550f747c5358bf
Daniel Danis <daniel.gordon.danis@protonmail.com>
2026-03-10 15:23:07 +0100

    Show HRA example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import HpoInputFormComponent from './hpo-input-form/hpo-input-form';
+import { HraExample } from "./hra-example/hra-example";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HpoInputFormComponent],
+  imports: [RouterOutlet, HpoInputFormComponent, HraExample],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/hra-example/hra-example.ts
+++ src/app/hra-example/hra-example.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-hra-example',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './hra-example.css',
 })
 export class HraExample {
-
+  // E.g. `UBERON:0001229` for renal corpuscle
+  uberon = input<string>("UBERON:0001229");
+  
+  // An array with Cell ontology CURIEs for highlighting the illustration substructures.
+  cellStructures = input<string[]>(["CL:123", "CL:456"]);
 }
